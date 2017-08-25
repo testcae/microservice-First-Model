dc9609dc8f0c8ae851bdc0cc9dc4782956a480f1
ukimiawz <deathmoon123>
2017-08-25 16:14:21 +0200

    Generated microservice version 1.0

---
+++ db/First_Model_create_tables.sql
@@ -0,0 +1,17 @@
+--
+-- Database Schema:  faqschema
+-- Automatically generated sql script for the service First Model, created by the CAE.
+-- --------------------------------------------------------
+
+--
+-- Table structure for table faq.
+--
+CREATE TABLE faqschema.faq (
+  question VARCHAR(255) ,
+  id INT NOT NULL AUTO_INCREMENT ,
+CONSTRAINT id_PK PRIMARY KEY (id)
+ 
+);
+
+
+
